8264f631a7ace65108a1c7329c8160eb2e5eda6e
Andjelko Stefanov <a.stefanov@piratetech.rs>
2025-01-01 20:00:37 +0100

    working ugly

--- app/Jobs/UpdateExternalPostsCache.php
+++ app/Jobs/UpdateExternalPostsCache.php
@@ -24,35 +24,38 @@ class UpdateExternalPostsCache implements ShouldQueue
         try {
             $response = $http
                 ->retry(3, 1000)
+                ->withHeader( 'timeout', 1000)
                 ->get(config('external.url'));
 
-            $xmlContent = (string) $response->getBody();
-            $xml = simplexml_load_string($xmlContent);
+            if ($response->successful()) {
+                $xmlContent = (string) $response->getBody();
+                $xml = simplexml_load_string($xmlContent);
 
-            $newPositions = [];
-            foreach ($xml->position as $position) {
-                $jobDescriptions = [];
-                if (! empty($position->jobDescriptions)) {
-                    foreach ($position->jobDescriptions->jobDescription as $jobDescription) {
-                        $jobDescriptions[] = [
-                            'name' => (string) $jobDescription->name,
-                            'value' => (string) $jobDescription->value,
-                        ];
+                $newPositions = [];
+                foreach ($xml->position as $position) {
+                    $jobDescriptions = [];
+                    if (! empty($position->jobDescriptions)) {
+                        foreach ($position->jobDescriptions->jobDescription as $jobDescription) {
+                            $jobDescriptions[] = [
+                                'name' => (string) $jobDescription->name,
+                                'value' => (string) $jobDescription->value,
+                            ];
+                        }
                     }
+
+                    $newPositions[] = [
+                        'id' => (string) $position->id,
+                        'office' => (string) $position->office,
+                        'name' => (string) $position->name,
+                        'jobDescriptions' => $jobDescriptions,
+                        'employmentType' => (string) $position->employmentType,
+                        'seniority' => (string) $position->seniority,
+                        'keywords' => (string) $position->keywords,
+                    ];
                 }
 
-                $newPositions[] = [
-                    'id' => (string) $position->id,
-                    'office' => (string) $position->office,
-                    'name' => (string) $position->name,
-                    'jobDescriptions' => $jobDescriptions,
-                    'employmentType' => (string) $position->employmentType,
-                    'seniority' => (string) $position->seniority,
-                    'keywords' => (string) $position->keywords,
-                ];
+                Cache::forever($cacheKey, $newPositions);
             }
-
-            Cache::forever($cacheKey, $newPositions);
         } catch (\Exception $e) {
             Log::error('Failed to update external posts cache: '.$e->getMessage());
         }
